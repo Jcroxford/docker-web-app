9431026e57850c64faaaf5e8b4311bd891d1f793
Jcroxford <takeflightsc@gmail.com>
2017-07-07 23:54:26 -0600

    setup basic dev workflow

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # docker-web-app
 a gui for running docker commands
+
+## dev instructions
+ssh: `git clone git@github.com:Jcroxford/docker-web-app.git`
+
+run npm install to install dependencies
+
+
+| Scripts              | Use                                           |
+|----------------------|-----------------------------------------------|
+| `npm start`          | start server locally                          |
+| `npm run dev`        | start persistent server locally using nodemon |
+| `npm test`           | run mocha test suite                          |
+| `npm run test-watch` | run persistent mocha test suite               |
---
+++ server/server.js
@@ -0,0 +1,37 @@
+// TODO: write tests for dockerCommands.js
+// TODO: update README
+// TODO: setup basic working rest api for docker commands
+// TODO: add index.html that uses vue/bootstrap/axios to provide a gui for 
+      // displaying docker data and eventual user interaction
+
+const express =  require('express')
+
+const {getDockerImages, getDockerContainers} = require('./utils/dockerCommands')
+
+const port  = process.env.PORT || 3000
+const app = express()
+
+// temporary route to confirm express is working
+app.get('/', (req, res) => {
+  res.send('Hey there, try visiting the route /docker/allthethings ;)')
+})
+app.get('/docker/allthethings', (req, res) => {
+  // Promise.all(getDockerImages(), getDockerContainers())
+  //   .then( (dockerInfo) => {
+  //     res.json(dockerInfo)
+  //   })
+  //   .catch((err) => {
+  //     console.log(err)
+  //     res.status(400).send()
+  //   })
+  getDockerImages()
+    .then((res) => res.json(res))
+    .catch((err) => res.status(400).send())
+})
+// end temporary
+
+// app.listen(port, () => console.log(`listening on port ${port}`))
+
+getDockerImages()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
